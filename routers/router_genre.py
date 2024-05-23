4cf12bedd83747e2f568fe3681a7ca050533d4d3
Pupupupupipu <vika566384@gmail.com>
2024-05-23 16:15:09 +0700

    with def add genre

--- routers/router_genre.py
+++ routers/router_genre.py
@@ -1,10 +1,24 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
+from sqlalchemy.sql import text
 from sqlalchemy.ext.asyncio import AsyncSession
 from db_connection import get_session
 from models.models import Genre
 
 genre_router = APIRouter(tags=['genre'], prefix='/genre')
+
+@genre_router.post('/')
+async def quick_add(
+        item: str,
+        db: AsyncSession = Depends(get_session)):
+    try:
+        query = text(item)
+        await db.execute(query)
+        await db.commit()
+        return "Genres added"
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=f"Произошла ошибка: {e}")
 
 @genre_router.get('/{id}')
 async def get_genre(id: int,
